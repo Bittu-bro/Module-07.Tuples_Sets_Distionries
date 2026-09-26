a54dac695e3ab9731f261354ca4b544936b166af
Bittu-bro <crea.sarcastics@gmail.com>
2026-09-26 11:34:50 +0530

    For Loop

---
+++ 12. for loop.py
@@ -0,0 +1,30 @@
+# Hey this is the 1st to use for loop function .......
+# # a >>>>>>>> 10 times
+# l1 = [2, 4.7 , [2,8,9] , 6 , "Bittu"]
+# print(l1)
+
+# # # we can print element one by one
+# # for x in l1:
+# #     print(x)
+
+
+# #     #we can assume any thing in place of 'x' 
+
+# # for B in l1:
+# #     print(B)
+
+# for x in [2, 4.7 , [2,8,9] , 6 , "Bittu"]:
+#     print(x)
+#    # we can use directily variable value
+
+# x = "Apple"
+# for i in x:
+#     print(i)
+
+
+
+for x in range (1,11): # including "1" & excluding "11"
+    print(x)
+
+for x in range (1,11): # including "1" & excluding "11"
+    print('Apple')
