6682896fc7ce8d42998d8f55f28f1fb5ec3888a5
Bittu-bro <crea.sarcastics@gmail.com>
2026-09-26 09:27:07 +0530

    Complete intro

---
+++ 01. Introducing tuples.py
@@ -0,0 +1,51 @@
+#tuple
+#(item1, item2, .......) 
+#t1 = ("python", 23, 3.5, True,[2,9,4,8],(7,2,4))
+
+
+#print(f"{t1}\n{type(t1)}\n")
+#l1 = list(t1)
+#print(f"\n{l1}\n{type(l1)}")
+
+
+# print(len(t1))
+# print([-1][-1])
+
+
+
+# you can store all data type same as same lists. but, main thing is you can't edit tuples
+
+# print(type((t1[0])))
+# print(type((t1[1])))
+# print(type((t1[2])))
+# print(type((t1[3])))
+# print(type((t1[4])))
+
+
+
+# most important thing you should remember.(aagar tum circle braket nahi lagaye to bhi oo as a tuple hi work karega)
+#t2 = "python", 23, 3.5, True,[2,9,4,8],(7,2,4)
+
+
+# print(f"{t1}\n{type(t1)}\n")
+# l1 = list(t1)
+# print(f"\n{l1}\n{type(l1)}")
+
+
+
+
+#list = ["python", 23, 3.5, True,[2,9,4,8]]
+# print(f"{list}\n{type(list)}\n")
+# t0 = tuple(list[-1])
+# print(f"\n{t0}\n{type(t0)}")
+
+
+
+
+
+#one more example
+fruits = ("mango", "banana", "popaya", "apple", "orenge")
+
+print(f"{fruits}\n{type(fruits)}\n")
+fruits = list(fruits)
+print(f"\n{fruits}\n{type(fruits)}")
