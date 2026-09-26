d3cdd3917b3c07730914e6548b433f22505e94ec
Bittu-bro <crea.sarcastics@gmail.com>
2026-09-26 09:28:17 +0530

    Opration with tuple

---
+++ 02. Basic operations on tuples.py
@@ -0,0 +1,41 @@
+# #+ (concatenation)
+# student1 = ("Bittu", 4253)
+# student2 = (73.9, 87.9, 68, 98.9)
+# student_details = student1 + student2
+# print(student_details)
+
+# #in & not in (membership)
+# print("Bittu" in student_details)
+# print(4253 in student_details)
+# print(320 in student_details)
+
+
+
+# #* (repetition)
+# edu = ("B.sc" , 3000)
+# fee = edu * 3
+# print(fee)
+
+
+
+# count(How many times persent in tuple)
+t1 = 3,14,6,3,9,3,5,3
+print(f"\'{t1.count(3)}\', Timeas persent in the tuple")
+print(f"\'{t1.count(5)}\', Timeas persent in the tuple")
+
+
+# index(to find the position of element)
+print(f"Position of element is: {t1.index(14)}")
+print(f"Position of element is: {t1.index(9)}")
+
+
+# max
+print(f"Max of all element is: {max(t1)}")
+
+# min
+print(f"Min of all element is: {min(t1)}")
+
+
+# sum
+print(f"Sum of all element is: {sum(t1)}")
+
